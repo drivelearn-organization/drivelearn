732b8c6b0c81c64a11eadb453b0774eed07ee0ac
Isuru-Maldeniya <isurumaldeniya@gmail.com>
2021-06-01 09:57:36 +0530

    payment done

--- drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/Payment.java
+++ drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/Payment.java
@@ -12,13 +12,63 @@ public class Payment {
     private double amount;
     private Date date;
 
-//    @ManyToOne
-//    @JsonBackReference
-//    @JoinColumn(name = "student_id",referencedColumnName = "stuId")
-//    private Student student;
-//
-//    @OneToOne
-//    @JoinColumn(name = "course_id",referencedColumnName = "courseId")
-//    private Cource cource;
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "student_id",referencedColumnName = "stuId")
+    private Student student;
 
+    @OneToOne
+    @JoinColumn(name = "course_id",referencedColumnName = "courseId")
+    private Cource cource;
+
+    public Payment() {
+    }
+
+    public Payment(int paymentId, double amount, Date date, Student student, Cource cource) {
+        this.paymentId = paymentId;
+        this.amount = amount;
+        this.date = date;
+        this.student = student;
+        this.cource = cource;
+    }
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Cource getCource() {
+        return cource;
+    }
+
+    public void setCource(Cource cource) {
+        this.cource = cource;
+    }
 }
