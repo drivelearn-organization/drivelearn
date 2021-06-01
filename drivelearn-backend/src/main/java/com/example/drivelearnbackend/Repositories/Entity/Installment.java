8856c234857e4021d3f7b8a318fd2b990e3191ac
Isuru-Maldeniya <isurumaldeniya@gmail.com>
2021-06-01 09:51:06 +0530

    instalment done

--- drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/Installment.java
+++ drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/Installment.java
@@ -15,13 +15,63 @@ public class Installment {
     private double amount;
     private Date date;
 
-//    @ManyToOne
-//    @JsonBackReference
-//    private Employee employee;
-//
-//    @ManyToOne
-//    @JsonBackReference
-//    @JoinColumn(name = "student_id",referencedColumnName = "stuId")
-//    private Student student;
+    @ManyToOne
+    @JsonBackReference
+    private Employee employee;
 
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "student_id",referencedColumnName = "stuId")
+    private Student student;
+
+    public Installment() {
+    }
+
+    public Installment(int installmentId, double amount, Date date, Employee employee, Student student) {
+        this.installmentId = installmentId;
+        this.amount = amount;
+        this.date = date;
+        this.employee = employee;
+        this.student = student;
+    }
+
+    public int getInstallmentId() {
+        return installmentId;
+    }
+
+    public void setInstallmentId(int installmentId) {
+        this.installmentId = installmentId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
--- drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/Student.java
+++ drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/Student.java
@@ -22,34 +22,165 @@ public class Student {
     private String password;
     private String contact;
 
-//    @OneToMany(mappedBy = "student")
-//    @JsonManagedReference
-//    private List<Feedback> feedbacks=new ArrayList<>();
-//
-//    @ManyToOne
-//    @JsonBackReference
-//    @JoinColumn(name = "brnch_id",referencedColumnName = "branchid")
-//    private Branch branch;
-//
-//    @OneToMany(mappedBy = "student")
-//    @JsonManagedReference
-//    private List<StuSession> stuSessionList=new ArrayList<>();
-//
-//    @OneToMany(mappedBy = "student")
-//    @JsonManagedReference
-//    private List<Cource> courceList=new ArrayList<>();
-//
-//    @OneToMany(mappedBy = "student")
-//    @JsonManagedReference
-//    private List<Payment> paymentList=new ArrayList<>();
-//
-//    @ManyToMany
-//    @JsonIgnore
-//    @JoinTable(
-//            name = "student_vehicle_type_assoc",
-//            joinColumns = @JoinColumn(name = "student_id",referencedColumnName = "stuId"),
-//            inverseJoinColumns = @JoinColumn(name = "type_id",referencedColumnName = "typeId")
-//    )
-//    private List<VechileType> vechileTypes=new ArrayList<>();
+    @OneToMany(mappedBy = "student")
+    @JsonManagedReference
+    private List<Feedback> feedbacks=new ArrayList<>();
 
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "brnch_id",referencedColumnName = "branchid")
+    private Branch branch;
+
+    @OneToMany(mappedBy = "student")
+    @JsonManagedReference
+    private List<StuSession> stuSessionList=new ArrayList<>();
+
+    @OneToMany(mappedBy = "student")
+    @JsonManagedReference
+    private List<Cource> courceList=new ArrayList<>();
+
+    @OneToMany(mappedBy = "student")
+    @JsonManagedReference
+    private List<Payment> paymentList=new ArrayList<>();
+
+    @ManyToMany
+    @JsonIgnore
+    @JoinTable(
+            name = "student_vehicle_type_assoc",
+            joinColumns = @JoinColumn(name = "student_id",referencedColumnName = "stuId"),
+            inverseJoinColumns = @JoinColumn(name = "type_id",referencedColumnName = "typeId")
+    )
+    private List<VechileType> vechileTypes=new ArrayList<>();
+
+    public Student() {
+    }
+
+    public Student(int stuId, Date registereddate, String idnum, String address, Date dob, String username, String password, String contact, List<Feedback> feedbacks, Branch branch, List<StuSession> stuSessionList, List<Cource> courceList, List<Payment> paymentList, List<VechileType> vechileTypes) {
+        this.stuId = stuId;
+        this.registereddate = registereddate;
+        this.idnum = idnum;
+        this.address = address;
+        Dob = dob;
+        this.username = username;
+        this.password = password;
+        this.contact = contact;
+        this.feedbacks = feedbacks;
+        this.branch = branch;
+        this.stuSessionList = stuSessionList;
+        this.courceList = courceList;
+        this.paymentList = paymentList;
+        this.vechileTypes = vechileTypes;
+    }
+
+    public int getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(int stuId) {
+        this.stuId = stuId;
+    }
+
+    public Date getRegistereddate() {
+        return registereddate;
+    }
+
+    public void setRegistereddate(Date registereddate) {
+        this.registereddate = registereddate;
+    }
+
+    public String getIdnum() {
+        return idnum;
+    }
+
+    public void setIdnum(String idnum) {
+        this.idnum = idnum;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDob() {
+        return Dob;
+    }
+
+    public void setDob(Date dob) {
+        Dob = dob;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public List<Feedback> getFeedbacks() {
+        return feedbacks;
+    }
+
+    public void setFeedbacks(List<Feedback> feedbacks) {
+        this.feedbacks = feedbacks;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
+
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
+
+    public List<StuSession> getStuSessionList() {
+        return stuSessionList;
+    }
+
+    public void setStuSessionList(List<StuSession> stuSessionList) {
+        this.stuSessionList = stuSessionList;
+    }
+
+    public List<Cource> getCourceList() {
+        return courceList;
+    }
+
+    public void setCourceList(List<Cource> courceList) {
+        this.courceList = courceList;
+    }
+
+    public List<Payment> getPaymentList() {
+        return paymentList;
+    }
+
+    public void setPaymentList(List<Payment> paymentList) {
+        this.paymentList = paymentList;
+    }
+
+    public List<VechileType> getVechileTypes() {
+        return vechileTypes;
+    }
+
+    public void setVechileTypes(List<VechileType> vechileTypes) {
+        this.vechileTypes = vechileTypes;
+    }
 }
