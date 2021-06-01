6657b586a1c83e9c115c5bb29f28db78cd20ca05
Isuru-Maldeniya <isurumaldeniya@gmail.com>
2021-06-01 10:04:57 +0530

    vehicleType done

--- drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/VechileType.java
+++ drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/VechileType.java
@@ -15,14 +15,72 @@ public class VechileType {
     private String typeName;
     private String priorRequirement;
 
-//    @ManyToOne
-//    @JsonBackReference
-//    @JoinColumn(name = "category_id", referencedColumnName = "categoryId")
-//    private VehicleCategory category;
-//
-//    @OneToMany(mappedBy = "vechileType")
-//    @JsonManagedReference
-//    private List<Vehicle> vehicleList=new ArrayList<>();
-//
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "category_id", referencedColumnName = "categoryId")
+    private VehicleCategory category;
 
+    @OneToMany(mappedBy = "vechileType")
+    @JsonManagedReference
+    private List<Vehicle> vehicleList=new ArrayList<>();
+
+    public VechileType() {
+    }
+
+    public VechileType(int typeId, int minimumTrainingTime, String typeName, String priorRequirement, VehicleCategory category, List<Vehicle> vehicleList) {
+        this.typeId = typeId;
+        this.minimumTrainingTime = minimumTrainingTime;
+        this.typeName = typeName;
+        this.priorRequirement = priorRequirement;
+        this.category = category;
+        this.vehicleList = vehicleList;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    public int getMinimumTrainingTime() {
+        return minimumTrainingTime;
+    }
+
+    public void setMinimumTrainingTime(int minimumTrainingTime) {
+        this.minimumTrainingTime = minimumTrainingTime;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getPriorRequirement() {
+        return priorRequirement;
+    }
+
+    public void setPriorRequirement(String priorRequirement) {
+        this.priorRequirement = priorRequirement;
+    }
+
+    public VehicleCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(VehicleCategory category) {
+        this.category = category;
+    }
+
+    public List<Vehicle> getVehicleList() {
+        return vehicleList;
+    }
+
+    public void setVehicleList(List<Vehicle> vehicleList) {
+        this.vehicleList = vehicleList;
+    }
 }
