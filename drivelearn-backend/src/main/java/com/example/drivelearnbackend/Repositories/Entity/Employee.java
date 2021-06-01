99401f7b4eb35e3878c47eb282f7f1afad25cf91
Isuru-Maldeniya <isurumaldeniya@gmail.com>
2021-06-01 08:27:19 +0530

    admin done

--- drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/Employee.java
+++ drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/Employee.java
@@ -22,27 +22,166 @@ public class Employee {
     private int isActive;
     private String username;
     private String password;
-//    @ManyToOne
-//    @JsonBackReference
-//    @JoinColumn(name = "admin_id",referencedColumnName = "adminId")
-//    private Admin admin;
-//
-//    @ManyToOne
-//    @JsonBackReference
-//    @JoinColumn(name = "branch_id", referencedColumnName = "branchid")
-//    private Branch branch;
-//
-//    @OneToMany(mappedBy = "employee")
-//    @JsonManagedReference
-//    List<Installment> installmentList=new ArrayList<>();
-//
-//    @OneToMany(mappedBy = "trainer")
-//    @JsonManagedReference
-//    List<Session> trainersSessionList=new ArrayList<>();
-//
-//    @OneToMany(mappedBy = "assigner")
-//    @JsonManagedReference
-//    List<Session> assinersSessionList=new ArrayList<>();
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "admin_id",referencedColumnName = "adminId")
+    private Admin admin;
 
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "branch_id", referencedColumnName = "branchid")
+    private Branch branch;
 
+    @OneToMany(mappedBy = "employee")
+    @JsonManagedReference
+    List<Installment> installmentList=new ArrayList<>();
+
+    @OneToMany(mappedBy = "trainer")
+    @JsonManagedReference
+    List<Session> trainersSessionList=new ArrayList<>();
+
+    @OneToMany(mappedBy = "assigner")
+    @JsonManagedReference
+    List<Session> assinersSessionList=new ArrayList<>();
+
+    public Employee(int empid, String moNumber, String emNumber, int role, String firstName, String lastName, String NID, int isActive, String username, String password, Admin admin, Branch branch, List<Installment> installmentList, List<Session> trainersSessionList, List<Session> assinersSessionList) {
+        this.empid = empid;
+        this.moNumber = moNumber;
+        this.emNumber = emNumber;
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.NID = NID;
+        this.isActive = isActive;
+        this.username = username;
+        this.password = password;
+        this.admin = admin;
+        this.branch = branch;
+        this.installmentList = installmentList;
+        this.trainersSessionList = trainersSessionList;
+        this.assinersSessionList = assinersSessionList;
+    }
+
+    public Employee() {
+    }
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public String getMoNumber() {
+        return moNumber;
+    }
+
+    public void setMoNumber(String moNumber) {
+        this.moNumber = moNumber;
+    }
+
+    public String getEmNumber() {
+        return emNumber;
+    }
+
+    public void setEmNumber(String emNumber) {
+        this.emNumber = emNumber;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNID() {
+        return NID;
+    }
+
+    public void setNID(String NID) {
+        this.NID = NID;
+    }
+
+    public int getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(int isActive) {
+        this.isActive = isActive;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
+
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
+
+    public List<Installment> getInstallmentList() {
+        return installmentList;
+    }
+
+    public void setInstallmentList(List<Installment> installmentList) {
+        this.installmentList = installmentList;
+    }
+
+    public List<Session> getTrainersSessionList() {
+        return trainersSessionList;
+    }
+
+    public void setTrainersSessionList(List<Session> trainersSessionList) {
+        this.trainersSessionList = trainersSessionList;
+    }
+
+    public List<Session> getAssinersSessionList() {
+        return assinersSessionList;
+    }
+
+    public void setAssinersSessionList(List<Session> assinersSessionList) {
+        this.assinersSessionList = assinersSessionList;
+    }
 }
--- drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/Feedback.java
+++ drivelearn-backend/src/main/java/com/example/drivelearnbackend/Repositories/Entity/Feedback.java
@@ -18,18 +18,121 @@ public class Feedback {
     private String searingBalance;
     private String cluchBalance;
     private String gear;
-//    @ManyToOne
-//    @JoinColumn(name = "trainer_id",referencedColumnName = "empid")
-//    private Employee trainer;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "Stu_session_id",referencedColumnName = "stuSessionId")
-//    private StuSession stuSession;
-//
-//    @ManyToOne
-//    @JsonBackReference
-//    @JoinColumn(name = "student_id",referencedColumnName = "stuId")
-//    private Student student;
+    @ManyToOne
+    @JoinColumn(name = "trainer_id",referencedColumnName = "empid")
+    private Employee trainer;
 
+    @ManyToOne
+    @JoinColumn(name = "Stu_session_id",referencedColumnName = "stuSessionId")
+    private StuSession stuSession;
 
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "student_id",referencedColumnName = "stuId")
+    private Student student;
+
+    public Feedback() {
+    }
+
+    public Feedback(int feedbackid, Date date, String start, String end, String reverse, String searingBalance, String cluchBalance, String gear, Employee trainer, StuSession stuSession, Student student) {
+        this.feedbackid = feedbackid;
+        this.date = date;
+        this.start = start;
+        this.end = end;
+        this.reverse = reverse;
+        this.searingBalance = searingBalance;
+        this.cluchBalance = cluchBalance;
+        this.gear = gear;
+        this.trainer = trainer;
+        this.stuSession = stuSession;
+        this.student = student;
+    }
+
+    public int getFeedbackid() {
+        return feedbackid;
+    }
+
+    public void setFeedbackid(int feedbackid) {
+        this.feedbackid = feedbackid;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getReverse() {
+        return reverse;
+    }
+
+    public void setReverse(String reverse) {
+        this.reverse = reverse;
+    }
+
+    public String getSearingBalance() {
+        return searingBalance;
+    }
+
+    public void setSearingBalance(String searingBalance) {
+        this.searingBalance = searingBalance;
+    }
+
+    public String getCluchBalance() {
+        return cluchBalance;
+    }
+
+    public void setCluchBalance(String cluchBalance) {
+        this.cluchBalance = cluchBalance;
+    }
+
+    public String getGear() {
+        return gear;
+    }
+
+    public void setGear(String gear) {
+        this.gear = gear;
+    }
+
+    public Employee getTrainer() {
+        return trainer;
+    }
+
+    public void setTrainer(Employee trainer) {
+        this.trainer = trainer;
+    }
+
+    public StuSession getStuSession() {
+        return stuSession;
+    }
+
+    public void setStuSession(StuSession stuSession) {
+        this.stuSession = stuSession;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
