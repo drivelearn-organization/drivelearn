0f36890c8f3fc47d69ae600975afb926cbe01f07
Isuru-Maldeniya <isurumaldeniya@gmail.com>
2021-06-07 19:55:17 +0530

    input style are ok

--- drivelearnmobile/componants/commoncomponants/Register.js
+++ drivelearnmobile/componants/commoncomponants/Register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Formik} from 'formik';
 import {RadioButton} from 'react-native-paper';
 import {
   Text,
@@ -6,7 +7,7 @@ import {
   Keyboard,
   ScrollView,
   ImageBackground,
-  StyleSheet, View,
+  StyleSheet, View, TouchableOpacity, TextInput,
 } from 'react-native';
 
 const Register = ({navigation}) => {
@@ -24,11 +25,52 @@ const Register = ({navigation}) => {
           {/*  onPress={() => setChecked('first')}*/}
           {/*/>*/}
           <View style={styles.headerView}>
-            <Text style={styles.headerText}>Sign In</Text>
+            <TouchableOpacity>
+              <Text style={styles.headerText}>Sign In</Text>
+            </TouchableOpacity>
+
           </View>
           <View>
             <ImageBackground source={require('../../asets/logo/Logo_Box.png')} style={styles.headerlogo}></ImageBackground>
           </View>
+          <View style={styles.topicView}>
+            <Text style={styles.topiccText}>Sign Up</Text>
+          </View>
+
+          {/*form*/}
+
+          <Formik initialValues={{fullname:'',username:''}} onSubmit={()=>{
+
+          }}>
+            {(props)=>(
+                <View style={styles.inputView}>
+
+                  {/*username*/}
+
+                  <TextInput
+                      placeholder={'Full Name'}
+                      style={styles.inputone}
+                      placeholderTextColor={'white'}
+                      value={props.values.fullname}
+                      onChangeText={props.handleChange('fullname')}
+                      // onBlur={props.handleChange('username')}
+                      // placeholderStyle={{color:'red'}}
+                  />
+
+
+
+                  <TextInput
+                      placeholder={'Username'}
+                      style={styles.inputone}
+                      placeholderTextColor={'white'}
+                      value={props.values.username}
+                      onChangeText={props.handleChange('username')}
+                      // onBlur={props.handleChange('username')}
+                      // placeholderStyle={{color:'red'}}
+                  />
+                </View>
+            )}
+          </Formik>
         </ImageBackground>
       </ScrollView>
     </TouchableWithoutFeedback>
@@ -38,7 +80,7 @@ const Register = ({navigation}) => {
 const styles = StyleSheet.create({
   bacStyleSheetkgroundImage: {
     width: '100%',
-    height: 200,
+    // height: 200,
   },
   headerText:{
     color:'white',
@@ -51,6 +93,24 @@ const styles = StyleSheet.create({
   headerlogo:{
     width:60,
     height: 60
+  },
+  topicView:{
+    width:'100%',
+    alignItems:'center'
+  },
+  topiccText:{
+    color: 'white',
+    fontSize:35,
+    padding: 10
+  },
+  inputView:{
+    alignItems: 'center',
+  },
+  inputone:{
+    color:'white',
+    borderBottomWidth:1,
+    borderBottomColor:'white',
+    width:250
   }
 });
 export default Register;
