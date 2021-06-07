a45d5bb78a6ecb4aa2b8a7b15afa35d4bd887923
Isuru-Maldeniya <isurumaldeniya@gmail.com>
2021-06-07 15:24:15 +0530

    paper library is added

--- drivelearnmobile/componants/commoncomponants/Register.js
+++ drivelearnmobile/componants/commoncomponants/Register.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import {Text} from "react-native";
+import {
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+  ImageBackground,
+  StyleSheet,
+} from 'react-native';
 
 const Register = ({navigation}) => {
-    return (
-        <Text>this is register</Text>
-    );
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <ScrollView>
+        <ImageBackground
+          source={require('../../asets/background/registerwallpaper.png')}
+          style={styles.bacStyleSheetkgroundImage}
+        />
+      </ScrollView>
+    </TouchableWithoutFeedback>
+  );
 };
 
+const styles = StyleSheet.create({
+  bacStyleSheetkgroundImage: {
+    width: '100%',
+    height: 200,
+  },
+});
 export default Register;
