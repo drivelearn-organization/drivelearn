5fe6190de812b35a32256f835dbd599a525d0744
AyshaIfra <ayshaifra15@gmail.com>
2021-06-10 22:56:56 +0530

    Check

--- drivelearn-web/src/App.js
+++ drivelearn-web/src/App.js
@@ -15,6 +15,16 @@ function App() {
         >
           Learn React
         </a>
+		
+		<a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+		
         <a
           className="App-link"
           href="https://reactjs.org"
