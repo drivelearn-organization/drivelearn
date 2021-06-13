b751c65e0f89e4e6a7977bb942527cf7b35a85ba
Hasani <65714939+kanthi97@users.noreply.github.com>
2021-06-13 18:39:17 +0530

    complete navbar

--- drivelearn-website/src/components/Navbar.js
+++ drivelearn-website/src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            TRVL
+            DRIVE
             <i class='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
