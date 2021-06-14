aaa90ffb11ffc4441a79027977a5426b6ad78838
Hasani <65714939+kanthi97@users.noreply.github.com>
2021-06-14 13:20:36 +0530

    add new

---
+++ drivelearn-web/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <Button variant="outline-light">Light</Button>
+    </div>
+  );
+}
+
+export default App;
