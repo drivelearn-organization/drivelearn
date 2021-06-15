5182a3b0a1227412df9a6b7555db901ffde0108c
AyshaIfra <ayshaifra15@gmail.com>
2021-06-16 03:03:00 +0530

    Website home page is added again

--- drivelearn-website/src/App.js
+++ drivelearn-website/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './homePage/Navbar';
+import Home from './homePage/pages/Home';
+import About from './homePage/pages/About';
+import Contacts from './homePage/pages/Contacts';
+import SignUp from './homePage/pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contacts' component={Contacts} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
     </div>
   );
 }
