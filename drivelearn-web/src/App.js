e8ef3c738bf0da8e03a6165334aa220fc6d86e44
Hasani <65714939+kanthi97@users.noreply.github.com>
2021-06-15 11:55:45 +0530

    complete nav bar

--- drivelearn-web/src/App.js
+++ drivelearn-web/src/App.js
@@ -2,13 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import React, { useState,useEffect } from 'react';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import AboutUs from './components/pages/AboutUs';
+import ContactUs from './components/pages/ContactUs';
 
 function App() {
+  const [showNav, setShownav] = useState(false);
+  const handlesetShownav = () => {
+    setShownav(!showNav);
+  }
   return (
-    <div className="App">
-      Hello
-      <Button variant="outline-light">Light</Button>
-    </div>
+    <>
+    <Router>
+        
+        <header ><i onClick={()=>setShownav(!showNav)} className="fas fa-bars" ></i></header> 
+      
+        {/* {showNav && <Navbar/>} */}
+        <Navbar show = {showNav}/>
+        <div className="main">
+          <Route path="/" exact={true} component={Home}/>
+          <Route path="/AboutUs" exact={true} component={AboutUs}/>
+          <Route path="/ContactUs" exact={true} component={ContactUs}/>
+        </div>
+      
+    </Router>
+    </>
+    
   );
 }
 
---
+++ drivelearn-web/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React, { useState,useEffect } from 'react';
+import './css/Navbar.css';
+import {Link} from 'react-router-dom';
+
+function Navbar({show}) {
+    return (
+        <div className={show ? 'sidenav active' : 'sidenav'}>
+           <ul className='NanIcon'>
+               <li>
+                    <Link to="/">Home</Link>
+               </li>
+               <li>
+                    <Link to="/AbutUs">About us</Link>
+               </li>
+               <li>
+                    <Link to="/ContactUs">Contact us</Link>
+               </li>
+           </ul> 
+        </div>
+    )
+}
+
+export default Navbar
