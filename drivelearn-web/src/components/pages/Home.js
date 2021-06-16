d3924aa1ce2311ae5e1362484be65a0f171734c1
Hasani <65714939+kanthi97@users.noreply.github.com>
2021-06-16 13:26:54 +0530

    complete chart

--- drivelearn-web/src/components/pages/Home.js
+++ drivelearn-web/src/components/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Barchart from './Barchart';
+import Card from './card';
 
 function Home() {
     return (
         <div>
+             <Card/>
             <Barchart/>
+           
         </div>
     )
 }
