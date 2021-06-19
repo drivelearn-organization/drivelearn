0242b1ef156dab9416baeddca714693c899b8e3e
Hasani <65714939+kanthi97@users.noreply.github.com>
2021-06-19 13:31:46 +0530

    change files

--- drivelearn-website/src/App.js
+++ drivelearn-website/src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
+import Navbar from './adminComponents/Navbar';
 import React, { useState,useEffect } from 'react';
 import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
-import Home from './components/pages/Home';
-import AboutUs from './components/pages/AboutUs';
-import ContactUs from './components/pages/ContactUs';
+import Home from './adminComponents/pages/Home';
+import AboutUs from './adminComponents/pages/AboutUs';
+import ContactUs from './adminComponents/pages/ContactUs';
 
 function App() {
   const [showNav, setShownav] = useState(false);
