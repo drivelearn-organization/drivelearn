46e46491db07c927f6b4869e8091195fb49448f1
Hasani <65714939+kanthi97@users.noreply.github.com>
2021-06-23 00:56:15 +0530

    add logo

---
+++ drivelearn-website/src/adminComponents/pages/PaymentDetails.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/PaymentDetails.css';
+
+function PaymentDetails() {
+    return (
+        <div className ="emp-card-container">
+        <div className ="fullCard">
+              <p>ghghghj</p>
+        </div>
+            
+        </div>
+    )
+}
+
+export default PaymentDetails;
