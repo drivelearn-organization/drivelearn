719304126ba79e511c5e28ecc8f0ab52342b9494
Hasani <65714939+kanthi97@users.noreply.github.com>
2021-06-24 21:05:01 +0530

    add payment

--- drivelearn-website/src/adminComponents/pages/PaymentDetails.js
+++ drivelearn-website/src/adminComponents/pages/PaymentDetails.js
@@ -4,6 +4,9 @@ import '../css/PaymentDetails.css';
 function PaymentDetails() {
     return (
         <div className ="emp-card-container">
+        <div className ="searchBox">
+             <p>search</p> 
+        </div>
         <div className ="fullCard">
              <p>Hasani Nimeshika</p> 
         </div>
