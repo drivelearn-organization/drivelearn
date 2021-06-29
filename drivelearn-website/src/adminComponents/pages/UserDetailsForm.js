4b22ca5dd424598de67e3ea1994ec81e377c1f15
Hasani <65714939+kanthi97@users.noreply.github.com>
2021-06-29 18:37:53 +0530

    add formm

--- drivelearn-website/src/adminComponents/pages/UserDetailsForm.js
+++ drivelearn-website/src/adminComponents/pages/UserDetailsForm.js
@@ -9,6 +9,36 @@ function UserDetailsForm() {
                     <lable className="dash-content__lable">First Name</lable>
                     <input type ="text" class="form__input"/>
                 </div>
+                <div className="dash-content__input">
+                    <lable className="dash-content__lable">Last Name</lable>
+                    <input type ="text" class="form__input"/>
+                </div>
+                <div className="dash-content__input">
+                    <lable className="dash-content__lable">Email</lable>
+                    <input type ="text" class="form__input"/>
+                </div>
+                <div className="dash-content__input">
+                    <lable className="dash-content__lable">Phone number</lable>
+                    <input type ="text" class="form__input"/>
+                </div>
+                <div className="dash-content__input">
+                    <lable className="dash-content__lable">Address</lable>
+                    <input type="textarea" class="form__input"/>
+                </div>
+                <div className="dash-content__input">
+                    <lable className="dash-content__lable">City</lable>
+                    <input type ="text" class="form__input"/>
+                </div>
+                <div className="dash-content__input">
+                    <lable className="dash-content__lable">User Role</lable>
+                    <select name="cars" class="form__input">
+                      
+                        <option value="">Instructor</option>
+                        <option value="">Branch Manegaer</option>
+                      
+                    </select>
+                </div>
+
             </form>
         </div>
     )
