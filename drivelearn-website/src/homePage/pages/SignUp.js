dd94a5a15c41a8071d91c658a98b9761d7f7623d
AyshaIfra <ayshaifra15@gmail.com>
2021-07-01 15:01:17 +0530

    Completed all the pages in home

--- drivelearn-website/src/homePage/pages/SignUp.js
+++ drivelearn-website/src/homePage/pages/SignUp.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import '../../App.css';
-import './../Contact.css';
+import './../Signup.css';
 
 function Contacts() {
   return (
     <div className="container">
-		 <div className="contact-box">
-			 <div className="left">
-			 <img src='images/phone.jpg' alt="Logo" width='100%' height='100%'/> 
-			 </div>
-			 <div className="right">
-				
-
-				<button className="button1">Send</button>
-		    </div>
+		  <div className="contact-box">
+			  <div className="left">
+			    <img src='images/phone.jpg' alt="Logo" width='100%' height='100%'/> 
+			  </div>
+			<div className="right">
+				<h1><big>Welcome to DriveLEARN!</big></h1>
+        <p>
+        Register Now To Take Driver Education Right From Your Own Home.
+        </p>
+        <br/>
+        <h4>Start Driving</h4>
+        <br/><br/>
+        <button className="install-app"><i class="fab fa-google-play"></i>&nbsp;&nbsp; Get it on Google play</button>
+        <br/><br/>
+        <button className="install-app"><i class="fab fa-apple"></i>&nbsp;&nbsp;Download on the Apple store</button>
+		  </div>
       </div>
     </div>
   );
