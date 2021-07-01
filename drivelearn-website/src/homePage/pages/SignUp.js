27dc40db45532ecdb8b7866d301699f7fdd86343
AyshaIfra <ayshaifra15@gmail.com>
2021-07-01 13:58:01 +0530

    inserted website sign up page

--- drivelearn-website/src/homePage/pages/SignUp.js
+++ drivelearn-website/src/homePage/pages/SignUp.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import '../../App.css';
+import './../Contact.css';
 
-export default function SignUp() {
-  return <h1 className='sign-up'>SIGNUP</h1>;
+function Contacts() {
+  return (
+    <div className="container">
+		 <div className="contact-box">
+			 <div className="left">
+			 <img src='images/phone.jpg' alt="Logo" width='100%' height='100%'/> 
+			 </div>
+			 <div className="right">
+				
+
+				<button className="button1">Send</button>
+		    </div>
+      </div>
+    </div>
+  );
+
 }
+
+export default Contacts;
