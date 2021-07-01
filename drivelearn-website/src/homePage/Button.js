8684f1890bb9b27aa2d407de482033ddcb051087
AyshaIfra <ayshaifra15@gmail.com>
2021-07-01 10:51:41 +0530

    Contact page is done

--- drivelearn-website/src/homePage/Button.js
+++ drivelearn-website/src/homePage/Button.js
@@ -1,12 +1,3 @@
-
-// export function Button() {
-//   return (
-//     <Link to='sign-up'>
-//       <button className='btn'>Sign Up</button>
-//     </Link>
-//   );
-// }
-
 import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
--- drivelearn-website/src/homePage/pages/Contacts.js
+++ drivelearn-website/src/homePage/pages/Contacts.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import '../../App.css';
+import './../Contact.css';
 
 function Contacts() {
-  return <h1 className='contacts'>CONTACTS</h1>;
+  return (
+    <div className="container">
+		 <div className="contact-box">
+			 <div className="left"></div>
+			 <div className="right">
+				<h2>Contact Us</h2>
+				<input type="text" class="field" placeholder="Your Name" />
+				<input type="text" class="field" placeholder="Your Email" />
+				<input type="text" class="field" placeholder="Phone" />
+				<textarea placeholder="Message" class="field"></textarea>
+				<button className="btn">Send</button>
+		    </div>
+      </div>
+    </div>
+  );
+
 }
 
 export default Contacts;
