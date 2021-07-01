ae8c2d73d523c91b1efc4f63cec655c5e654150e
AyshaIfra <ayshaifra15@gmail.com>
2021-07-01 09:01:38 +0530

    Insert about page

--- drivelearn-website/src/homePage/pages/About.js
+++ drivelearn-website/src/homePage/pages/About.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import '../../App.css';
+import './../AboutBody.css';
 
 function About() {
   return (
-    <>
-      
-     
-    </>
+    <div className="about-section">
+    <div className="inner-container">
+        <h1>About Us</h1>
+        <p className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit ducimus, enim inventore earum, eligendi nostrum pariatur necessitatibus eius dicta a voluptates sit deleniti autem error eos totam nisi neque voluptates sit deleniti autem error eos totam nisi neque.
+        </p>
+        <div className="skills">
+            {/* <span>Web Design</span>
+            <span>Photoshop & Illustrator</span>
+            <span>Coding</span> */}
+        </div>
+    </div>
+    </div>
+
   );
 }
 
