2564ac97b7267ffaaa81f7a00f584fde98bd549e
AyshaIfra <ayshaifra15@gmail.com>
2021-07-01 13:12:27 +0530

    Completely done homePage

--- drivelearn-website/src/homePage/pages/About.js
+++ drivelearn-website/src/homePage/pages/About.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../../App.css';
-import './../AboutBody.css';
+import './../About.css';
 
 function About() {
   return (
     
-    <div className="about-section">
-    <div className="inner-container">
+    <div className="container">
+    <div className="contact-box">
         <h1><big>Over 20 Years of Excellence in Drivers Education</big></h1>
-        <p className="text">
+        <div className="right">
+        <h2>About</h2>
+        <p>
         Official Driving School has provided superior driver education in Kalutara 
         for more than 20 years under the DriveLEARN Driving School brand. 
         Our programs are fully certified by the State 
@@ -16,6 +18,7 @@ function About() {
         rigorous training and background checks, and training is in safe, late-model vehicles. 
         Our classroom are conveniently located in schools and at many DriveLEARN retail locations.
         </p>
+        <br/><br/>
         <h3>Official Driving School Core Values</h3>
         <ul>
         <li>We care</li>
@@ -24,7 +27,7 @@ function About() {
         <li>We do what we say with urgency, honesty and integrity</li>
         <li>We strive for excellence because what we do is important</li>
         </ul>
-
+        </div>
     </div>
     </div>
 
--- drivelearn-website/src/homePage/pages/Contacts.js
+++ drivelearn-website/src/homePage/pages/Contacts.js
@@ -6,14 +6,16 @@ function Contacts() {
   return (
     <div className="container">
 		 <div className="contact-box">
-			 <div className="left"></div>
+			 <div className="left">
+			 <img src='images/image1.jpeg' alt="Logo" width='100%' height='100%'/> 
+			 </div>
 			 <div className="right">
 				<h2>Contact Us</h2>
 				<input type="text" class="field" placeholder="Your Name" />
 				<input type="text" class="field" placeholder="Your Email" />
 				<input type="text" class="field" placeholder="Phone" />
 				<textarea placeholder="Message" class="field"></textarea>
-				<button className="btn">Send</button>
+				<button className="button1">Send</button>
 		    </div>
       </div>
     </div>
