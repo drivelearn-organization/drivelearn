4ee905bec9b111196f3eacc491242d84157b9fa3
ebp1996 <ebp.perera@gmail.com>
2021-07-06 18:48:33 +0530

    Create the AddVehicles.js file
    I have created Add new vehicals information form.

---
+++ drivelearn-website/src/Branch-AddVehicles/AddVehicles.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import './AddVehicles.css';
+
+
+const AddVehicles = () => {
+    return (
+        <div className='container'>
+            <div className='vehical-form'>
+                <h2 style={{ marginTop: "30px", marginLeft: "40px" }}>Add Vehical</h2>
+                <form>
+                    <div className='double-colum'>
+                        <div className='a'>
+                            <div className='detail-input'>
+                                <label>Vehical No</label>
+
+                                <div className='input-feild'>
+                                    <input type='text' />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='b'>
+                            <div className='detail-input'>
+                                <label>Starting Mileage</label>
+
+                                <div className='input-feild'>
+                                    <input type='text' />
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className='double-colum'>
+                        <div className='a'>
+                            <div className='detail-input'>
+                                <label>Register Date</label>
+
+                                <div className='input-feild'>
+                                    <input type='text' />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='b'>
+                            <div className='detail-input'>
+                                <label>Vehical type</label>
+
+                                <div className='input-feild'>
+                                    <input type='text' />
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div className='a' style={{ width: "42%" }}>
+                        <div className='detail-input'>
+                            <label>Branch</label>
+
+                            <div className='input-feild'>
+                                <input type='text' />
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className='a' style={{ width: "42%" }} >
+                        <div className='detail-input'>
+                            <label>Chassis Number</label>
+
+                            <div className='input-feild'>
+                                <input type='text' />
+                            </div>
+                        </div>
+                    </div>
+
+                    <h4 style={{ marginLeft: "10px", marginTop: "20px", textAlign: "center" }}>Register Details</h4>
+                    <div>
+                        <div className='double-colum'>
+                            <div className='a'>
+                                <div className='detail-input'>
+                                    <label>License starting date</label>
+
+                                    <div className='input-feild'>
+                                        <input type='date' />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='b'>
+                                <div className='detail-input'>
+                                    <label>License expire date</label>
+
+                                    <div className='input-feild'>
+                                        <input type='date' />
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+
+                        <div className='double-colum'>
+                            <div className='a'>
+                                <div className='detail-input'>
+                                    <label>Insurance starting date</label>
+
+                                    <div className='input-feild'>
+                                        <input type='date' />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='b'>
+                                <div className='detail-input'>
+                                    <label>Insurance expire date</label>
+
+                                    <div className='input-feild'>
+                                        <input type='date' />
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                    <div style={{ position: "relative" }}>
+                        <input className="submit-btn" type='submit' value='Add' />
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default AddVehicles;
