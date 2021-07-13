2eefb4777e306b2b0954ce0afa1867bf3f47939c
ebp1996 <ebp.perera@gmail.com>
2021-07-13 09:35:42 +0530

    form validation add UpdateVehicle.js file
    add form validation functions to the this file.

--- drivelearn-website/src/Branch-AddVehicles/UpdateVehicles.js
+++ drivelearn-website/src/Branch-AddVehicles/UpdateVehicles.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './AddVehicles.css';
+import Validation from './AddVehicleErrors';
 
 
-const UpdateVehicles = () => {
+const UpdateVehicles = ({submitForm}) => {
+
+    const [value, setValue] = useState({
+        VehicleNo: "",
+        StartingMileage: "",
+        RegisterDate: "",
+        VehicleType: "",
+        Branch: "",
+        ChessisNo: "",
+        L_StartingDate: "",
+        L_ExpireDate: "",
+        I_StartingDate: "",
+        I_ExpireDate: "",
+
+    });
+
+    const [errors, setErrors] = useState({});
+    const [dataIsCorrect, setDataIsCorrect] = useState(false);
+
+    const handleChange = (event) => {
+        setValue({
+            ...value,
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        setErrors(Validation(value));
+        setDataIsCorrect(true);
+    }
+
+    useEffect(() => {
+        if (Object.keys(errors).length === 0 && dataIsCorrect) {
+            submitForm(true);
+        }
+    }, [errors]);
+
     return (
         <div>
             <div className='vehical-form'>
@@ -11,20 +49,30 @@ const UpdateVehicles = () => {
                     <div className='double-colum'>
                         <div className='a'>
                             <div className='detail-input'>
-                                <label>Vehical No</label>
+                                <div className="error-warp">
+                                    <label>Vehical No</label>
+                                    {errors.VehicleNo && <p className='error'>{errors.VehicleNo}</p>}
+                                </div>
+
 
                                 <div className='input-feild'>
-                                    <input type='text' />
+                                    <input type='text' name='VehicleNo' value={value.VehicleNo} onChange={handleChange} />
+
                                 </div>
                             </div>
                         </div>
 
                         <div className='b'>
                             <div className='detail-input'>
-                                <label>Starting Mileage</label>
+                                <div className="error-warp">
+                                    <label>Starting Mileage</label>
+                                    {errors.StartingMileage && <p className='error'>{errors.StartingMileage}</p>}
+                                </div>
+
 
                                 <div className='input-feild'>
-                                    <input type='text' />
+                                    <input type='text' name='StartingMileage' value={value.StartingMileage} onChange={handleChange} />
+
                                 </div>
                             </div>
                         </div>
@@ -34,20 +82,28 @@ const UpdateVehicles = () => {
                     <div className='double-colum'>
                         <div className='a'>
                             <div className='detail-input'>
-                                <label>Register Date</label>
+                                <div className="error-warp">
+                                    <label>Register Date</label>
+                                    {errors.RegisterDate && <p className='error'>{errors.RegisterDate}</p>}
+                                </div>
+
 
                                 <div className='input-feild'>
-                                    <input type='text' />
+                                    <input type='text' name='RegisterDate' value={value.RegisterDate} onChange={handleChange} />
+
                                 </div>
                             </div>
                         </div>
 
                         <div className='b'>
                             <div className='detail-input'>
-                                <label>Vehical type</label>
-
+                                <div className='error-warp'>
+                                    <label>Vehical type</label>
+                                    {errors.VehicleType && <p className='error'>{errors.VehicleType}</p>}
+                                </div>
                                 <div className='input-feild'>
-                                    <input type='text' />
+                                    <input type='text' name='VehicleType' value={value.VehicleType} onChange={handleChange} />
+
                                 </div>
                             </div>
                         </div>
@@ -58,20 +114,28 @@ const UpdateVehicles = () => {
                     <div className='double-colum'>
                         <div className='a'>
                             <div className='detail-input'>
-                                <label>Branch</label>
+                                <div className="error-warp">
+                                    <label>Branch</label>
+                                    {errors.Branch && <p className='error'>{errors.Branch}</p>}
+                                </div>
+
 
                                 <div className='input-feild'>
-                                    <input type='text' />
+                                    <input type='text' name='Branch' value={value.Branch} onChange={handleChange} />
+
                                 </div>
                             </div>
                         </div>
 
                         <div className='b'>
                             <div className='detail-input'>
-                                <label>Chessis No</label>
-
+                                <div className="error-warp">
+                                    <label>Chessis No</label>
+                                    {errors.ChessisNo && <p className='error'>{errors.ChessisNo}</p>}
+                                </div>
                                 <div className='input-feild'>
-                                    <input type='text' />
+                                    <input type='text' name='ChessisNo' value={value.ChessisNo} onChange={handleChange} />
+
                                 </div>
                             </div>
                         </div>
@@ -83,20 +147,28 @@ const UpdateVehicles = () => {
                         <div className='double-colum'>
                             <div className='a'>
                                 <div className='detail-input'>
-                                    <label>License starting date</label>
+                                    <div className="error-warp">
+                                        <label>License starting date</label>
+                                        {errors.L_StartingDate && <p className='error'>{errors.L_StartingDate}</p>}
+                                    </div>
 
                                     <div className='input-feild'>
-                                        <input type='date' />
+                                        <input type='date' name='L_StartingDate' value={value.L_StartingDate} onChange={handleChange} />
+
                                     </div>
                                 </div>
                             </div>
 
                             <div className='b'>
                                 <div className='detail-input'>
-                                    <label>License expire date</label>
+                                    <div className="error-warp">
+                                        <label>License expire date</label>
+                                        {errors.L_ExpireDate && <p className='error'>{errors.L_ExpireDate}</p>}
+                                    </div>
 
                                     <div className='input-feild'>
-                                        <input type='date' />
+                                        <input type='date' name="L_ExpireDate" value={value.L_ExpireDate} onChange={handleChange} />
+
                                     </div>
                                 </div>
                             </div>
@@ -107,20 +179,27 @@ const UpdateVehicles = () => {
                         <div className='double-colum'>
                             <div className='a'>
                                 <div className='detail-input'>
-                                    <label>Insurance starting date</label>
-
+                                    <div className="error-warp">
+                                        <label>Insurance starting date</label>
+                                        {errors.I_StartingDate && <p className='error'>{errors.I_StartingDate}</p>}
+                                    </div>
                                     <div className='input-feild'>
-                                        <input type='date' />
+                                        <input type='date' name='I_StartingDate' value={value.I_StartingDate} onChange={handleChange} />
+
                                     </div>
                                 </div>
                             </div>
 
                             <div className='b'>
                                 <div className='detail-input'>
-                                    <label>Insurance expire date</label>
+                                    <div className="error-warp">
+                                        <label>Insurance expire date</label>
+                                        {errors.I_ExpireDate && <p className='error'>{errors.I_ExpireDate}</p>}
+                                    </div>
 
                                     <div className='input-feild'>
-                                        <input type='date' />
+                                        <input type='date' name='I_ExpireDate' value={value.I_ExpireDate} onChange={handleChange} />
+
                                     </div>
                                 </div>
                             </div>
@@ -131,7 +210,7 @@ const UpdateVehicles = () => {
                     </div>
 
                     <div style={{ position: "relative" }}>
-                        <input className="submit-btn" type='submit' value='Update' />
+                        <input className="submit-btn" type='submit' value='Update' onClick={handleFormSubmit} />
                     </div>
 
                 </form>
