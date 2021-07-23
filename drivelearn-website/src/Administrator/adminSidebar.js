bf32aba916e130705912b5ce793205266c6ab9b2
AyshaIfra <ayshaifra15@gmail.com>
2021-07-23 20:46:43 +0530

    Added dashboards

---
+++ drivelearn-website/src/Administrator/adminSidebar.js
@@ -0,0 +1,75 @@
+import "./../BranchManager/Sidebar.css";
+
+const Sidebar = ({sidebarOpen, closeSidebar}) => {  
+      return(
+      <div className={sidebarOpen ? "sidebar-responsive" : ""} id="sidebar">
+      <div className="sidebar__title">
+        <div className="sidebar__img">
+        <img src='images/LogoWhite.png' alt="Logo" width="50" height="50"/>
+          <h1>DriveLEARN</h1>
+        </div>
+        <div className="closeicon" onClick={ () => closeSidebar()}>
+        <i
+          className="fa fa-times"
+        ></i>
+        </div>
+      </div>
+
+      <div className="sidebar__menu">
+      <div className="sidebar__link active_menu_link">
+          <i className="fa fa-home"></i>
+          <a href="./">Home</a>
+        </div>
+        <div className="sidebar__link active_menu_link">
+          <i className="fa fa-tachometer"></i>
+          <a href="./branchmanager">Dashboard</a>
+        </div>
+        <h2>MANAGEMENT</h2>
+        <div className="sidebar__link">
+          <i className="fa fa-users" aria-hidden="true"></i>
+          <a href="#">Students</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-user-secret"></i>
+          <a href="#">Branch Managers</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-user-secret"></i>
+          <a href="#">Instructors</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-bus"></i>
+          <a href="#">Vehicles</a>
+        </div>
+        <div className="sidebar__link">
+          <i className="fa fa-cc-paypal"></i>
+          <a href="#">Payments</a>
+        </div>
+        {/* <div className="sidebar__link">
+          <i className="fa fa-clock-o"></i>
+          <a href="#">Training Sessions</a>
+        </div> */}
+        <h2>PROFILE</h2>
+        <div className="sidebar__link">
+          <i className="fa fa-cog"></i>
+          <a href="#">Settings</a>
+        </div>
+        {/* <div className="sidebar__link">
+          <i className="fa fa-user-plus"></i>
+          <a href="#">Edit Profile</a>
+        </div> */}
+        <div className="sidebar__logout">
+          <i className="fa fa-power-off"></i>
+          <a href="./">Logout</a>
+        </div>
+        <div className="sidebar__logout">
+          <i className="fa fa-times-circle"></i>
+          <a href="#">Deactivate</a>
+        </div>
+      </div>
+    </div>
+);
+
+}
+
+export default Sidebar;
--- drivelearn-website/src/App.js
+++ drivelearn-website/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './HomePage/pages/Home';
+// import Footer from './homePage/Footer';
+import Login from './HomePage/pages/Login';
+import Signup from './HomePage/pages/Signup';
+import Administrator from './Administrator/pages/adminDashboard';
+import BranchManager from './BranchManager/pages/managerDashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={Signup} />
+          <Route path='/admin' component={Administrator} />
+          <Route path='/branchmanager' component={BranchManager} />
+        </Switch>
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
 }
