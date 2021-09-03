16d6f8e1854fd30aee5398b139ec424fea56797b
ebp1996 <ebp.perera@gmail.com>
2021-09-03 22:32:02 +0530

    Update Vehicle controller
    Add cross origin annotation

--- drivelearn-backend/src/main/java/com/example/drivelearnbackend/Controllers/VehicleController.java
+++ drivelearn-backend/src/main/java/com/example/drivelearnbackend/Controllers/VehicleController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.LinkedList;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping(value = "vehicle")
 public class VehicleController {
     @Autowired
