814c601ca474131a1993bc1452cb5d88bd6015aa
Hansa1997$ <60069417+H-Sandaruwan@users.noreply.github.com>
2021-09-08 22:47:55 +0530

    update scrips

--- Game/Assets/Script/CarSteer.cs
+++ Game/Assets/Script/CarSteer.cs
@@ -1,4 +1,4 @@
-/* using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -132,4 +132,4 @@ public class CarSteer : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
 
 
 
-} */
+}   
