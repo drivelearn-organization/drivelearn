05c57658712685c2bb12821b442188693fb98889
Hasani <65714939+kanthi97@users.noreply.github.com>
2021-09-14 21:18:54 +0530

    complete add settings

--- drivelearn-website/src/Administrator/pages/adminSetting.js
+++ drivelearn-website/src/Administrator/pages/adminSetting.js
@@ -5,10 +5,24 @@ import './../../BranchManager/Profile.css';
 import Navbar from './../../BranchManager/Navbar';
 import Sidebar from './../adminSidebar';
 import axios from 'axios';
+import Errorbox from './errorbox';
+import SuccessfulyMsgBox from './successfulyMsgBox'
 
 const AdminSettings = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [getData, setGetData] = useState([]);
+  const [toggle, settoggle] = useState(false);
+  const [newtoggle, setnewtoggle] = useState(true);
+  const [disabled, setDisabled] = useState(true);
+  const [modal, setModal] = useState(false);
+  const [Msg, setMsg] = useState({
+      errorMsg: ''
+    });
+
+  const [errMsg, seterrMsg] = useState({
+      errorMsg: ''
+    });
+
   const[state, setState] = useState({
     
     username: sessionStorage.getItem('username'),
@@ -68,13 +82,32 @@ const[newPassword, setNewPassword] = useState({
       ...state,
       [e.target.name]: e.target.value
   }) 
-
+  setDisabled(false);
 }
 
 const handleSubmit = (e) =>{
   e.preventDefault()
   axios.post('http://localhost:8080/drivelearn/settingMyProfile',state)
-  console.log(state);
+  .then(response => {
+      
+    if(response.data === "Updated successfully")
+    {
+      console.log(response.data);
+      settoggle(true);
+      setMsg({
+        errorMsg: response.data
+      })
+      window.location = '/administrator';
+      
+    }else{
+     
+      setModal(true);
+      seterrMsg({
+        errorMsg: response.data
+      })
+    }
+  })
+  
  
 }
 
@@ -83,12 +116,31 @@ const handleChangePassword = (e) => {
       ...newPassword,
       [e.target.name]: e.target.value
   }) 
-
+  setnewtoggle(false);
 }
 
 const handleSubmitPassword = (e) =>{
   e.preventDefault()
   axios.post('http://localhost:8080/drivelearn/settingMyProfilePassword',newPassword)
+  .then(response => {
+      
+    if(response.data === "Updated successfully")
+    {
+      console.log(response.data);
+      settoggle(true);
+      setMsg({
+        errorMsg: response.data
+      })
+      window.location = '/administrator';
+      
+    }else{
+     
+      setModal(true);
+      seterrMsg({
+        errorMsg: response.data
+      })
+    }
+  })
   
  
 }
@@ -98,6 +150,8 @@ return (
   <div className="container">
       <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
       <main>
+      {modal && <Errorbox closeModal={setModal} errorMsg={errMsg}/>}
+      {toggle && <SuccessfulyMsgBox closeModal={settoggle} errorMsg={Msg}/>}
     <div className="main__container">
       {/* <!-- MAIN TITLE STARTS HERE --> */}
 
@@ -138,7 +192,7 @@ return (
            <form className="charts__right__cardss" onSubmit={handleSubmitPassword}>
             <div className="card-p">
                 <p className="text">Current Password</p>
-                <input className="data" type="password" name="password" id="firstname" placeholder="Current Password" value={newPassword.password} onChange={handleChangePassword}/>
+                <input className="data" type="password" name="password" id="firstname" placeholder="Current Password" value={newPassword.password} onChange={handleChangePassword} required/>
                 {/* <div class="alert-danger" id="firstNameError">
                    * First name can't be empty and must contain only letters
                 </div> */}
@@ -146,7 +200,7 @@ return (
             
             <div className="card-p">
                 <p className="text">New password</p>
-                <input className="data" type="password" name="password2" id="firstname" placeholder="New Password" value={newPassword.password2} onChange={handleChangePassword}/>
+                <input className="data" type="password" name="password2" id="firstname" placeholder="New Password" value={newPassword.password2} onChange={handleChangePassword} required/>
                 {/* <div class="alert-danger" id="firstNameError">
                    * First name can't be empty and must contain only letters
                 </div> */}
@@ -154,13 +208,13 @@ return (
            
             <div className="card-p">
                 <p className="text">Confirm Password</p>
-                <input className="data" type="Password" name="password3" id="firstname" placeholder="Confirm Password" value={newPassword.password3} onChange={handleChangePassword}/>
+                <input className="data" type="Password" name="password3" id="firstname" placeholder="Confirm Password" value={newPassword.password3} onChange={handleChangePassword} required/>
                 {/* <div class="alert-danger" id="firstNameError">
                    * First name can't be empty and must contain only letters
                 </div> */}
             </div>
             <center>
-            <input type="submit" value="Update" className="update-btn" />
+            <input type="submit" value="Update" className="update-btn" disabled={newtoggle} style = {{ opacity: newtoggle ? "0.7":"1"}}/>
             &nbsp;&nbsp;&nbsp;
             <input type="Reset" value="Clear" className="reset1-btn" />
             </center>
@@ -216,16 +270,9 @@ return (
             </div>
           
           
-            <div className="card-p">
-                <p className="text">Branch</p>
-                <select className="data" type="text"  name="branch" id="firstname" placeholder="DOB"  value={state.branch} onChange={handleChange}  required >
-                <option value ="mathugama">mathugama</option>
-                <option value ="kaluthatara">kaluthatara</option>
-                <option value ="Aluthgama">Aluthgama</option>
-                </select>
-            </div>
+           
             <center>
-            <input type="submit" value="Update" className="update-btn" />
+            <input type="submit" value="Update" className="update-btn" disabled={disabled} style = {{ opacity: disabled ? "0.7":"1"}}/>
             &nbsp;&nbsp;&nbsp;
             <input type="Reset" value="Clear" className="reset1-btn" />
             </center>
