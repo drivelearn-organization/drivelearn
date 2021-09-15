126b23adada92b9085c9a1f4e8539bd82874489e
AyshaIfra <ayshaifra15@gmail.com>
2021-09-16 01:44:59 +0530

    Debud in admin part

--- drivelearn-web/src/components/pages/Home.js
+++ drivelearn-web/src/components/pages/Home.js
@@ -33,31 +33,3 @@ function Home() {
 
 export default Home;
 
-function Home() {
-    return (
-        <div>
-             <Card/>
-             <div className = "charContent">
-              <div className ="studentChart">
-              <div className = "studentChart-content">
-                Monthly Registerd Student 2021
-              </div>
-            
-              <Barchart/>
-              </div>
-              <div className = "incomeChart">
-              <div className = "incomeChart-content">
-                  Monthly Income 2021
-              </div>
-             
-              <Barchart/>
-              </div>
-
-             </div>
-           
-           
-        </div>
-    )
-}
-
-export default Home;
