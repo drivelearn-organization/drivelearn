aa5fd0a192654cbe859d14b20b895531489e4246
AyshaIfra <ayshaifra15@gmail.com>
2021-09-16 01:34:16 +0530

    Some UIs changes and added new UIs

--- drivelearn-web/src/App.js
+++ drivelearn-web/src/App.js
@@ -8,9 +8,7 @@ import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
 import Home from './components/pages/Home';
 import AboutUs from './components/pages/AboutUs';
 import ContactUs from './components/pages/ContactUs';
-import Home from './components/pages/Home';
-import AboutUs from './components/pages/AboutUs';
-import ContactUs from './components/pages/ContactUs';
+
 
 function App() {
   const [showNav, setShownav] = useState(false);
