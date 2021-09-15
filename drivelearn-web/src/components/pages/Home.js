e2879585f2ee9cd0447c53492db83196a46d29f5
AyshaIfra <ayshaifra15@gmail.com>
2021-09-16 01:44:15 +0530

    Popup Added

--- drivelearn-web/src/components/pages/Home.js
+++ drivelearn-web/src/components/pages/Home.js
@@ -3,6 +3,36 @@ import Barchart from './Barchart';
 import Card from './card';
 import '../css/Home.css';
 
+
+function Home() {
+    return (
+        <div>
+             <Card/>
+             <div className = "charContent">
+              <div className ="studentChart">
+              <div className = "studentChart-content">
+                Monthly Registerd Student 2021
+              </div>
+            
+              <Barchart/>
+              </div>
+              <div className = "incomeChart">
+              <div className = "incomeChart-content">
+                  Monthly Income 2021
+              </div>
+             
+              <Barchart/>
+              </div>
+
+             </div>
+           
+           
+        </div>
+    )
+}
+
+export default Home;
+
 function Home() {
     return (
         <div>
