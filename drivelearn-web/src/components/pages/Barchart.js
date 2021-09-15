98c67c58be392cc423ad547e5144ca58450fc47f
AyshaIfra <ayshaifra15@gmail.com>
2021-09-16 01:41:17 +0530

    Debug

--- drivelearn-web/src/components/pages/Barchart.js
+++ drivelearn-web/src/components/pages/Barchart.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Bar} from 'react-chartjs-2';
 import '../css/Barchart.css';
+mport React from 'react';
+import {Bar} from 'react-chartjs-2';
+import '../css/Barchart.css';
 
 const Barchart = () => {
     return (
